0012b391010f8e3b5d6bcd2d9332ce5abe1bc88b
davidlee <david.wushi@foxmail.com>
2024-06-08 17:13:34 +0800

    Initial commit

---
+++ Provider/src/main/java/wushi/cn/Controller/UserController.java
@@ -0,0 +1,28 @@
+package wushi.cn.Controller;
+
+import org.springframework.web.bind.annotation.*;
+import wushi.cn.Entity.CommonResult;
+import wushi.cn.Entity.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getUserById/{id}")
+    public CommonResult<User> getUserById(@PathVariable("id") Integer id) {
+        try {
+            User user = new User(id, "小米", "123456");
+            return new CommonResult<>(200, "success11000", user);
+        } catch (Exception e) {
+            return new CommonResult<>(500, "error", null);
+        }
+    }
+    @GetMapping("/getUserByName/{name}")
+    public CommonResult<User> getUserByName(@PathVariable("name") String name) {
+        try {
+            User user = new User(1, name, "123456");
+            return new CommonResult<>(200, "success11000", user);
+        } catch (Exception e) {
+            return new CommonResult<>(500, "error", null);
+        }
+    }
+}
---
+++ Provider11001/src/main/java/wushi/cn/Controller/UserController.java
@@ -0,0 +1,31 @@
+package wushi.cn.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wushi.cn.Entity.CommonResult;
+import wushi.cn.Entity.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getUserById/{id}")
+    public CommonResult<User> getUserById(@PathVariable("id") Integer id) {
+        try {
+            User user = new User(id, "小米", "123456");
+            return new CommonResult<>(200, "success11001", user);
+        } catch (Exception e) {
+            return new CommonResult<>(500, "error", null);
+        }
+    }
+    @GetMapping("/getUserByName/{name}")
+    public CommonResult<User> getUserByName(@PathVariable("name") String name) {
+        try {
+            User user = new User(1, name, "123456");
+            return new CommonResult<>(200, "success11001", user);
+        } catch (Exception e) {
+            return new CommonResult<>(500, "error", null);
+        }
+    }
+}
